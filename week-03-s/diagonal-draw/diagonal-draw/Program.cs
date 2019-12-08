c0d30c9a49683e4230e01cd044063680832e3632
Petra <pierrett75@gmail.com>
2019-12-08 19:03:57 +0100

    new exercise

---
+++ week-03-s/diagonal-draw/diagonal-draw/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace diagonal_draw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+            int i, n;
+            Console.WriteLine("tell me number of lines:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(new String('%', n));
+
+            for (i = 1; i <= n - 2; i++)
+            {
+                Console.WriteLine("%" + new String(' ', n - 2) + "%");
+            }
+            Console.WriteLine(new String('%', n));
+        }
+    }
+}
---
+++ week-03-s/guess-the-number/guess-the-number/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace guess_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+            int n,i;
+        start:
+            Console.WriteLine("guess the number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+           string answer = " ";
+            
+            if (n > 8)
+            {
+                Console.WriteLine("the stored number is lower");
+                Console.WriteLine("Do you want try it again ? yes/no");
+                answer = Console.ReadLine();
+                switch(answer)
+                {
+                    case "yes":
+                       
+                        goto start;
+                    case "no":
+                        Console.WriteLine("end");
+                        break;
+                    default:
+                        break;
+
+                }
+               
+            }
+            else if (n<8)
+            {
+                Console.WriteLine("the stored number is lower");
+                Console.WriteLine("Do you want try it again ? yes/no");
+                answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "yes":
+
+                        goto start;
+                    case "no":
+                        Console.WriteLine("end");
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            else if (n==8)
+            {
+                Console.WriteLine("You found the number: 8");
+            }
+        }
+    }
+}
