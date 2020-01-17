63d17013ba781ad0de12a6d90260642659ea7a2c
Petra <pierrett75@gmail.com>
2020-01-17 21:11:38 +0100

    new exercise

---
+++ week-07-f/Substrlist/Substrlist/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Substrlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a function that takes a string and a list of string as a parameter
+            //  Returns the index of the string in the list where the first string is part of
+            //  Only need to find the first occurence and return the index of that
+            //  Returns `-1` if the string is not part any of the strings in the list
+
+            //  Example
+            string[] searchArr = { "this", "is", "what", "I'm", "searching", "in" };
+
+            Substrlist("ching", searchArr);
+            //  should print: `4`
+            Substrlist("not", searchArr);
+            //  should print: `-1`  
+        }
+        static void Substrlist(string name, string[] array)
+        {
+
+
+            int index2 = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Contains(name))
+                {
+                    index2 = i;
+                    break;
+                }
+            }
+
+
+            Console.WriteLine(index2);
+        }
+    }
+}
---
+++ week-07-f/heNeverWill/heNeverWill/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace heNeverWill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string output = "";
+            int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
+
+            checkPairs(notSoCrypticMessage);
+
+            // Things are a little bit messed up
+            // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+            // Assemble the fragments into the out variable
+
+
+        }
+        static void checkPairs(int[] input)
+        {
+            StringBuilder output = new StringBuilder();
+            var map = new Dictionary<int, string>();
+
+            map.Add(7, "run around and desert you");
+            map.Add(50, "tell a lie and hurt you ");
+            map.Add(49, "make you cry, ");
+            map.Add(2, "let you down");
+            map.Add(12, "give you up, ");
+            map.Add(1, "Never gonna ");
+            map.Add(11, "\n");
+            map.Add(3, "say goodbye ");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (map.ContainsKey(input[i]))
+                {
+                    output.Append(map[input[i]]);
+                }
+
+            }
+            Console.WriteLine(output);
+        }
+
+    }
+}
