14574216d14e883aaae75d5072199cf3828423f0
Petra <pierrett75@gmail.com>
2020-01-23 17:30:24 +0100

    new exercise go to center

---
+++ week-09-f/drawing/DrawingApp/DrawingApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GreenFox;
+
+namespace DrawingApp
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+
+
+            // foxDraw.DrawLine(x, y, 150, 150);
+
+            DrawCenter(10, 20);
+
+
+        }
+
+        public void DrawCenter(int x, int y)
+        {
+
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            for (int i = 0; i < 4; i++)
+            {
+                foxDraw.DrawLine((x + (i * 20)), y, 150, 150);
+            }
+
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+    }
+}
