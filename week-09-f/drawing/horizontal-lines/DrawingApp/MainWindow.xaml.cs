fb89879bfe0572c53b081b2f8f1a898cbe123a1d
Petra <pierrett75@gmail.com>
2020-01-23 18:48:12 +0100

    add triangle and rectangles exercise

--- week-09-f/drawing/horizontal-lines/DrawingApp/MainWindow.xaml.cs
+++ week-09-f/drawing/horizontal-lines/DrawingApp/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using GreenFox;
+using System.Collections.Generic;
 
 namespace DrawingApp
 {
@@ -16,21 +18,53 @@ namespace DrawingApp
             var canvas = this.Get<Canvas>("canvas");
             var foxDraw = new FoxDraw(canvas);
 
-
-
             // foxDraw.DrawLine(x, y, 150, 150);
 
             //DrawCenter(10, 20);
-            DrawLine(10, 20, 50);
 
+
+            DrawRectangle(140, 155, 140);
+
+
+        }
+        public void DrawRectangle(int x, int z, int v)
+        {
+            // draw four different size and color rectangles.
+            // avoid code duplication.
+
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            for (int i = 0; i < 4; i++)
+            {
+                var points = new List<Point>();
+                points.Add(new Point(x + (i * 20), x + (i * 20)));
+                points.Add(new Point(z + (i * 20), z + (i * 20)));
+                points.Add(new Point(v + (i * 20), z + (i * 20)));
+
+                foxDraw.SetFillColor(Colors.Green);
+                foxDraw.DrawPolygon(points);
+            }
+
+        }
+
+        //public void Squere()
+        //{
+        //    var canvas = this.Get<Canvas>("canvas");
+        //    var foxDraw = new FoxDraw(canvas);
+        //    // Draw a green 10x10 square to the canvas' center.
+        //    foxDraw.SetStrokeColor(Colors.Green);
+        //    foxDraw.DrawLine(145, 145, 155, 145);
+        //    foxDraw.DrawLine(155, 145, 155, 155);
+        //    foxDraw.DrawLine(155, 155, 145, 155);
+        //    foxDraw.DrawLine(145, 155, 145, 145);
+        //}
+        public void DrawLine(int x, int y, int z)
+        {
             // Create a function that draws a single line and takes 3 parameters:
             // The x and y coordinates of the line's starting point and the foxDraw
             // and draws a 50 long horizontal line from that point.
             // Draw at least 3 lines with that function using a loop.
-        }
-        public void DrawLine(int x, int y, int z)
-        {
-
+            //DrawLine(10, 20, 50);
             var canvas = this.Get<Canvas>("canvas");
             var foxDraw = new FoxDraw(canvas);
             for (int i = 0; i < 4; i++)
