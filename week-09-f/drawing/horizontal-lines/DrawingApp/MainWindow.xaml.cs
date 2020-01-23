efbf3cc3a717cb19fc6df651e9bc0ed85a475a04
Petra <pierrett75@gmail.com>
2020-01-23 18:14:05 +0100

    horizontal exercise

---
+++ week-09-f/drawing/horizontal-lines/DrawingApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GreenFox;
+
+namespace DrawingApp
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+
+
+            // foxDraw.DrawLine(x, y, 150, 150);
+
+            //DrawCenter(10, 20);
+            DrawLine(10, 20, 50);
+
+            // Create a function that draws a single line and takes 3 parameters:
+            // The x and y coordinates of the line's starting point and the foxDraw
+            // and draws a 50 long horizontal line from that point.
+            // Draw at least 3 lines with that function using a loop.
+        }
+        public void DrawLine(int x, int y, int z)
+        {
+
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            for (int i = 0; i < 4; i++)
+            {
+                foxDraw.DrawLine(x, (y + (i * 10)), (x + z), (y + (i * 10)));
+            }
+
+        }
+        public void DrawCenter(int x, int y)
+        {
+
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            for (int i = 0; i < 4; i++)
+            {
+                foxDraw.DrawLine((x + (i * 20)), y, 150, 150);
+            }
+
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+    }
+}
