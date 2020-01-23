ab0c94e6991f515965d1c7f80f15e1e258d37e11
Petra <pierrett75@gmail.com>
2020-01-23 15:38:09 +0100

    add drawing box

---
+++ week-09-f/drawing/colored-box/colored-box/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Shapes;
+
+namespace colored_box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // Draw a box that has different colored lines on each edge.
+            // Add a Line Element
+            var myLine = new Line();
+            myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+            myLine.X1 = 1;
+            myLine.X2 = 50;
+            myLine.Y1 = 1;
+            myLine.Y2 = 50;
+            myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            myLine.VerticalAlignment = VerticalAlignment.Center;
+            myLine.StrokeThickness = 2;
+        }
+    }
+}
