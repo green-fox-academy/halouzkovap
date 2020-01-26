baf1f6aad5a4635823ac717dc1df5f338d85cce4
Petra <pierrett75@gmail.com>
2020-01-26 05:07:48 +0100

    file with using

--- week-8-s/FileRead/FileRead/Program.cs
+++ week-8-s/FileRead/FileRead/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FileRead
 {
     class Program
     {
+        static void StreamReader()
+        {
+            StreamReader sr = new StreamReader("Value.txt");//new object read every line
+            string line = "";// we put stream to line
+
+            while (line != null)
+            {
+                line = sr.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            sr.Close();
+        }
         static void Main(string[] args)
         {
+            string[] lines = { "This is line 2", "This is line 3", "this is line 4" };
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\42073\Temp\shity.txt"))
+            {
+                foreach (var line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
