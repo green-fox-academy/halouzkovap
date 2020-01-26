60a02d3e5b00fbbc88771b04fa26903974c2b4e8
Petra <pierrett75@gmail.com>
2020-01-26 04:18:29 +0100

    new file with learnig file manipulation

---
+++ week-8-s/FileManipulatinEx/FileLearn/FileLearn/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Text;
+
+namespace FileLearn
+{
+    class Program
+    {
+        static void WriteText(string fileName)
+        {
+            File.WriteAllText(fileName, "hello word");
+        }
+
+        static void ReadText(string fileName)
+        {
+            System.Console.WriteLine(File.ReadAllText(fileName));
+
+        }
+
+        static void WriteFS(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            if (fs.CanWrite)
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes("encodujeme ascii a dostanu byty");
+                fs.Write(buffer, 0, buffer.Length);
+            }
+
+            fs.Flush();
+            fs.Close();
+        }
+        static void ReadFS(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            if (fs.CanRead)
+            {
+                byte[] buffer = new byte[1025];
+                int readBuffer = fs.Read(buffer, 0, buffer.Length);
+
+                System.Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, readBuffer));
+            }
+
+
+            fs.Flush();
+            fs.Close();
+        }
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\42073\Temp\shity.txt";
+            WriteText(fileName);
+            ReadText(fileName);
+
+            string fileName2 = @"C:\Users\42073\Temp\shityFileStream";
+            WriteFS(fileName2);
+            ReadFS(fileName2);
+            System.Console.ReadLine();
+        }
+    }
+}
