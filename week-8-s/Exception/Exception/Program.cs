6aad9b4fc198b8c0047d5fb3d911fae85e968960
Petra <pierrett75@gmail.com>
2020-01-26 05:43:13 +0100

    learning file with exception

---
+++ week-8-s/Exception/Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //int x = 1;
+                //int y = 0;
+                //double z = x / y;
+                //Console.WriteLine(z);
+                Console.WriteLine("hello word");
+                throw new SystemException("goodbye ");
+            }
+            catch (System.Exception zeroError)
+            {
+
+                Console.WriteLine(zeroError.Message);
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
