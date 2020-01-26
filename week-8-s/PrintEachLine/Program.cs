670767deeb892d95af1f68f332c9ad0606c05033
Petra <pierrett75@gmail.com>
2020-01-26 07:03:24 +0100

    new ex. PrintEachLine

---
+++ week-8-s/PrintEachLine/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each line from the file.
+            // If the program is unable to read the file (for example it does not exist),
+            // then it should print the following error message: "Unable to read file: my-file.txt"
+            try
+            {
+                using (StreamReader sr = new StreamReader("Value.txt"))
+                {
+                    string line = "";// we put stream to line
+
+                    while (line != null)
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                    sr.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Unable to read file: my-file.txt");
+                // Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
