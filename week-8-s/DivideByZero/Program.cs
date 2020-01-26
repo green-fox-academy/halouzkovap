c8d91262859f1533923e6040e359b11aebbc1fb0
Petra <pierrett75@gmail.com>
2020-01-26 06:40:58 +0100

    ex. divideByZero

---
+++ week-8-s/DivideByZero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static string Divide(string num)
+        {
+            int numB = Convert.ToInt32(num);
+            int a = 10;
+            int result;
+
+            try
+            {
+                result = a / numB;
+                return Convert.ToString(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                string message = "you cannot divide by zero";
+                return message;
+
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divide("0"));
+
+            Console.ReadLine();
+        }
+    }
+}
