f5bb6369c7e8b7ca8ff33de37d3c100c2929cf26
Petra <pierrett75@gmail.com>
2020-01-28 18:23:50 +0100

    revese line ex

---
+++ week-09-f/ReverseLine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ReverseLine
+{
+    class Program
+    {
+        public static void ReverseLine(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+
+            string line;
+
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                char[] myArr = line.ToCharArray();
+                Array.Reverse(myArr);
+
+                Console.WriteLine(myArr);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\halouzkova\source\repos\ReverseLine\reverse.txt";
+
+            ReverseLine(path);
+
+            Console.ReadLine();
+        }
+    }
+}
