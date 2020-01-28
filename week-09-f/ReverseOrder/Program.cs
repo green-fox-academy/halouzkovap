add1b366c6fcb38f55a597f31315c34c77519b5e
Petra <pierrett75@gmail.com>
2020-01-28 18:24:25 +0100

    reverse order ex

---
+++ week-09-f/ReverseOrder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ReverseOrder
+{
+    class Program
+    {
+        public static void ReverseLine(string path)
+        {
+            var reverseLine = File.ReadAllLines(path).Reverse();
+            foreach (var item in reverseLine)
+            {
+                Console.WriteLine(item);
+            }
+            //StreamReader sr = new StreamReader(path);
+
+            //string line;
+
+            //StringBuilder newOrder = new StringBuilder();
+
+            //while ((line = sr.ReadLine()) != null)
+            //{
+            //    newOrder.Insert(0, line);
+            //}
+            //Console.WriteLine(newOrder);
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\halouzkova\source\repos\ReverseOrder\ReverseOrders.txt";
+            ReverseLine(path);
+
+            Console.ReadLine();
+        }
+    }
+}
