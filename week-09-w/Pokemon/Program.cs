2fc8e5f40284996129d67f582a93e523058c18d1
Petra <pierrett75@gmail.com>
2020-01-28 21:19:51 +0100

    pokemon ex.

---
+++ week-09-w/Pokemon/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pokemon> pokemonOfAsh = InitializePokemon();
+
+            // Every pokemon has a name and a type.
+            // Certain types are effective against others, e.g. water is effective against fire.
+
+            // Ash has a few pokemon.
+            // A wild pokemon appeared!
+
+            Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
+
+            // Which pokemon should Ash use?
+            foreach (var item in pokemonOfAsh)
+            {
+
+                if (item.IsEffectiveAgainst(wildPokemon))
+                {
+                    Console.Write("I choose you, {0}", item.Name);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        private static List<Pokemon> InitializePokemon()
+        {
+            return new List<Pokemon>
+            {
+                new Pokemon("Balbasaur", "leaf", "water"),
+                new Pokemon("Pikatchu", "electric", "water"),
+                new Pokemon("Charizard", "fire", "leaf"),
+                new Pokemon("Balbasaur", "water", "fire"),
+                new Pokemon("Kingler", "water", "fire")
+            };
+        }
+    }
+    class Pokemon
+    {
+        public String Name;
+        public String Type;
+        public String EffectiveAgainst;
+
+        public Pokemon(String name, String type, String effectiveAgainst)
+        {
+            Name = name;
+            Type = type;
+            EffectiveAgainst = effectiveAgainst;
+        }
+
+        public bool IsEffectiveAgainst(Pokemon anotherPokemon)
+        {
+            return EffectiveAgainst == anotherPokemon.Type;
+        }
+    }
+}
