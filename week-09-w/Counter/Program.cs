d63cdf7633e73c5323495b221c3f2a130ee16c3b
Petra <pierrett75@gmail.com>
2020-01-28 20:53:03 +0100

    counter ex.

---
+++ week-09-w/Counter/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numA = new Counter();
+            numA.num = 10;
+
+            var numB = new Counter(9);
+        }
+    }
+
+    class Counter
+    {
+        public int num { get; set; }
+
+        public Counter()
+        {
+            num = 0;
+        }
+
+        public Counter(int num)
+            : this()
+        { }
+        public void Add()
+        {
+            num++;
+        }
+
+        public void Get()
+        {
+            num.ToString();
+        }
+
+        public void Reset()
+        {
+            num = 0;
+        }
+    }
+}
