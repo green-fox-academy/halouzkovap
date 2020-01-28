a46c4379c297202fa1294bea498164192b125107
Petra <pierrett75@gmail.com>
2020-01-28 20:40:45 +0100

    animal ex.

---
+++ week-09-w/Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal1 = new Animal();
+            Console.WriteLine(animal1.Hunger + animal1.Thirst);
+
+            animal1.eat();
+            Console.WriteLine(animal1.Hunger + animal1.Thirst);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Animal
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+        public Animal()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+        public void eat()
+        {
+            Hunger--;
+        }
+        public void drink()
+        {
+            Thirst--;
+        }
+        public void play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+
+    }
+}
--- week-8-s/CopyFile/Program.cs
+++ week-8-s/CopyFile/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CopyFile
+﻿namespace CopyFile
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+            // It should take the filenames as parameters
+            // It should return a boolean that shows if the copy was successful
         }
     }
 }
