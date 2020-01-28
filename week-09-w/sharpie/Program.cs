07393b1022503faeeb8726bd208a51873aeb5825
Petra <pierrett75@gmail.com>
2020-01-28 20:46:20 +0100

    sharpie ex.

---
+++ week-09-w/sharpie/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharpie1 = new Sharpie("Green", 5.030f);
+            sharpie1.Use();
+        }
+    }
+    class Sharpie
+    {
+        public string Color { get; set; }
+        public float Width { get; set; }
+        public float InkAmount { get; set; }
+
+        public Sharpie(string Color, float width)
+        {
+            InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+    }
+}
