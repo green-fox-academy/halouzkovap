c2b3fe8994438dcf27fd86057e5aa5d70b6094f0
Petra <pierrett75@gmail.com>
2020-02-29 09:08:24 +0100

    Extension Test

---
+++ week-13-s/Sharpie/SharpieTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Sharpies
+{
+    public class SharpieTest
+    {
+        [Fact]
+        public void Use()
+        {
+            Sharpies sharpie = new Sharpies("yellow", 5.0f);
+            sharpie.Use();
+            float result = sharpie.InkAmount;
+            Assert.Equal(99, result);
+
+        }
+    }
+}
---
+++ week-13-s/Sharpie/Sharpies.cs
@@ -0,0 +1,22 @@
+﻿namespace Sharpies
+{
+    public class Sharpies
+    {
+
+        public string Color { get; set; }
+        public float Width { get; set; }
+        public float InkAmount { get; set; }
+        public Sharpies(string color, float width)
+        {
+            Color = color;
+            Width = width;
+            InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+
+    }
+}
