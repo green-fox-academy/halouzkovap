99237f92d89d7643542d562eef442aedabe2f0a7
Petra <pierrett75@gmail.com>
2020-04-14 17:35:07 +0200

    linq ex.

---
+++ LinqEx10/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqEx10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Exercise 10
+            //Create a Fox class with 3 properties: name, type and color.
+
+            //Fill a list with at least 5 foxes, it's up to you how you name/create them.
+
+            var fox1 = new Fox("Pet", "junior", "green");
+            var fox2 = new Fox("Pet", "pallida", "yellow");
+            var fox3 = new Fox("Pet", "junior", "red");
+            var fox4 = new Fox("Pet", "junior", "lila");
+            var fox5 = new Fox("Kam", "pallida", "green");
+            List<Fox> Foxes = new List<Fox>();
+            Foxes.Add(fox1);
+            Foxes.Add(fox2);
+            Foxes.Add(fox3);
+            Foxes.Add(fox4);
+            Foxes.Add(fox5);
+
+            //Write a LINQ Expression to find the foxes with green color.
+            var greenOne = Foxes.FindAll(x => x.Color == "green").ToList();
+            //Write a LINQ Expression to find the foxes with green color and pallida type.
+            var greenAndPalidaOne = Foxes.FindAll(x => x.Color == "green" && x.Type == "pallida").ToList();
+            foreach (var item in greenOne)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+            foreach (var item in greenAndPalidaOne)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+
+
+
+        }
+    }
+
+    class Fox
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+
+        public Fox(string name, string type, string color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+
+        }
+
+    }
+}
---
+++ LinqEx12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinqEx12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the name of the heaviest character(if the mass is unknown, ignore that character)
+            //Print the average height of the male characters
+            //Print the average height of the female characters
+            //💪 Get the age distribution of the characters by gender(where the gender can be "male", "female" and "other")
+            //The age groups are: "below 21", "between 21 and 40", "above 40" and "unknown"
+            //The result should be a Dictionary<string, Dictionary<string, int>> similar to this one:
+
+            string path = @"C:\Users\42073\halouzkovap\starWarst.csv";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(';');
+                foreach (string column in columns)
+                {
+                    System.Console.WriteLine(column);
+                }
+            }
+        }
+    }
+}
