4f83f24ed49f0b6a7fd09c71c3a610d7c9290781
Petra <pierrett75@gmail.com>
2020-04-14 21:12:27 +0200

    try to solve 12ex now i can read file, next solve linq

--- LinqEx12/Program.cs
+++ LinqEx12/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace LinqEx12
+﻿using LumenWorks.Framework.IO.Csv;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LinqEx12
 {
     class Program
     {
@@ -11,16 +15,27 @@
             //The age groups are: "below 21", "between 21 and 40", "above 40" and "unknown"
             //The result should be a Dictionary<string, Dictionary<string, int>> similar to this one:
 
-            string path = @"C:\Users\42073\halouzkovap\starWarst.csv";
-            string[] lines = System.IO.File.ReadAllLines(path);
-            foreach (string line in lines)
+            //CsvFile.DefaultCsvDefinition.FieldSeparator = '\t';
+            var csvTable = new System.Data.DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead("starWarst.csv")), true))
             {
-                string[] columns = line.Split(';');
-                foreach (string column in columns)
-                {
-                    System.Console.WriteLine(column);
-                }
+                csvTable.Load(csvReader);
             }
+            List<Star> searchParameters = new List<Star>();
+            for (int i = 0; i < csvTable.Rows.Count; i++)
+            {
+                searchParameters.Add(new Star { Name = csvTable.Rows[i][0].ToString(), Height = csvTable.Rows[i][1].ToString(), Mass = csvTable.Rows[i][2].ToString(), hair_color = csvTable.Rows[i][3].ToString(), skin_color = csvTable.Rows[i][4].ToString(), eye_color = csvTable.Rows[i][5].ToString(), birth_year = csvTable.Rows[i][6].ToString(), gender = csvTable.Rows[i][7].ToString() });
+            }
+
+            //Print the name of the heaviest character(if the mass is unknown, ignore that character))
+
+            //Print the average height of the male characters
+            //Print the average height of the female characters
+
+            //foreach (var item in searchParameters)
+            //{
+            //    System.Console.WriteLine(item.Name);
+            //}
         }
     }
 }
