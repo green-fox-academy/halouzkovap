e20ffce85f5c23553ea5fca4fda31a503bfa0a79
Petra <pierrett75@gmail.com>
2020-04-16 16:54:04 +0200

    MVC ADD

---
+++ MvcStart/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStart.Models;
+
+namespace MvcStart.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var pirate = new Pirate("Petra", "Captain");
+            return View(pirate);
+        }
+    }
+}
---
+++ MvcStart/Models/HiCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcStart.Models
+{
+    public class HiCollection : IHiCollection
+    {
+        public IEnumerable<Hello> hellow => new List<Hello> {
+
+        new Hello{ Hi="Hello"},
+        new Hello{ Hi="Hallo"},
+        new Hello{ Hi="Qanuipit"},
+        new Hello{ Hi="Hola"},
+        new Hello{ Hi="Nei Ho"},
+        new Hello{ Hi="Jambo"},
+        new Hello{ Hi="Bonjour"},
+        new Hello{ Hi="Hello"},
+        new Hello{ Hi="Hello"},
+        };
+
+
+
+        public Hello GetHello(int id)
+        {
+            return hellow.ElementAt(id);
+        }
+    }
+}
